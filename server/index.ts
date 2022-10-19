664f1c72e04bf6475eb11ed961d7ce30b9e1292f
Yusef Nsar <yusef.nsar@epicore.co>
2022-10-19 06:49:21 +0200

    feat: setup express

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello server!');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+console.log('Starting Server...');
+app.listen(PORT, () => {
+  console.log(`Started listening on ${PORT}`);
+});
