4cb2d0e83857e29b4d0f8cc71ff13832c5efe3d0
Yusef Nsar <yusef.nsar@epicore.co>
2022-10-19 22:40:06 +0200

    feat: add app router to express app

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { appRouter } from './src/routers/app.router';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/', appRouter);
 
 console.log('Starting Server...');
 app.listen(PORT, () => {
