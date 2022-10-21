7935400247b40d04332c73c0a9709e04f438bd5b
Yusef Nsar <yusef.nsar@epicore.co>
2022-10-21 06:08:10 +0200

    feat: finalize app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { ChildrenOutletContexts, Router } from '@angular/router';
+import { routesAnimation } from './route-animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [routesAnimation],
 })
 export class AppComponent {
   title = 'part-of-speech';
+  avatars = ['boy', 'girl', 'dad', 'dude'];
+  chosenAvatar = '';
+
+  constructor(
+    private contexts: ChildrenOutletContexts,
+    private router: Router
+  ) {
+    // in case user went to other routes directly
+    // or reloaded from them
+    if (this.chosenAvatar === '') {
+      this.router.navigateByUrl('/');
+    }
+
+    this.subscribeToRouter();
+  }
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+      'animation'
+    ];
+  }
+
+  onAvatarClick(chosenAvatar: string) {
+    this.chosenAvatar = chosenAvatar;
+  }
+
+  private subscribeToRouter(): void {
+    this.router.events.subscribe(() => {
+      const navigation = this.router.getCurrentNavigation();
+      const reset: boolean = navigation?.extras.state?.['reset'];
+
+      if (reset) {
+        this.chosenAvatar = '';
+      }
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PlayGroundComponent } from './play-ground/play-ground.component';
+import { RankPageComponent } from './rank-page/rank-page.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
     HomePageComponent,
     PlayGroundComponent,
+    RankPageComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,6 +25,6 @@ import { PlayGroundComponent } from './play-ground/play-ground.component';
     HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
