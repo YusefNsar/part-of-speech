150db528684c7e064e4059b402cd13cf59f80530
Yusef Nsar <yusef.nsar@epicore.co>
2022-10-21 06:04:04 +0200

    feat: add route informations

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PlayGroundComponent } from './play-ground/play-ground.component';
+import { RankPageComponent } from './rank-page/rank-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent, data: { animation: 'HomePage' } },
+  {
+    path: 'play-ground',
+    component: PlayGroundComponent,
+    data: { animation: 'PlayGround' },
+  },
+  {
+    path: 'rank-page',
+    component: RankPageComponent,
+    data: { animation: 'RankPage' },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
