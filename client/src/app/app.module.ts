bee61baf9a11f9b88ce6ebdde4b960484a307297
Yusef Nsar <yusef.nsar@epicore.co>
2022-10-21 06:06:15 +0200

    feat: add home page component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PlayGroundComponent } from './play-ground/play-ground.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomePageComponent,
+    PlayGroundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
